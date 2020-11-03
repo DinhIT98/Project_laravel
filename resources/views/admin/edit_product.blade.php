bcfbbcbdbb9a425858b028e75c3d0ac55d5a3518
minhgit commit -am ud <ndinh725@gmail.com>
2020-11-03 10:55:49 +0700

    fix  path

--- resources/views/admin/edit_product.blade.php
+++ resources/views/admin/edit_product.blade.php
@@ -1,4 +1,4 @@
-@extends('admin\layout\master')
+@extends('admin/layout/master')
 @section('content')
 <!-- <link href="//netdna.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
 <script src="//netdna.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"></script>
